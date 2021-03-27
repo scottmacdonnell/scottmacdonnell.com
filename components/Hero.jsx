791b9fe01e8aa14f48322cc3bdacf8b549117c9b
Scott MacDonnell <scottsmacdonnell@gmail.com>
2021-03-27 12:08:42 -0400

    Add animation to Hero component

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,19 +1,32 @@
+import { motion } from 'framer-motion'
+
 import Container from './Container'
 import Social from './Social'
 
 import styles from '../styles/components/Hero.module.scss'
+
+const transition = {
+  duration: 1.25,
+  ease: [0.43, 0.13, 0.23, 0.96],
+}
+
+const pageVariants = {
+  exit: { opacity: 0, transition },
+  enter: { opacity: 1, transition: { delay: 0.75, ...transition } },
+}
 
 export default function Hero() {
   return (
     <div className={styles.Hero}>
       <div className={styles.HeroContent}>
         <Container>
-          <div className={styles.Desktop}>
+          <motion.div className={styles.Desktop} variants={pageVariants}>
             <span><p>Hi, my name is Scott MacDonnell</p></span>
-          </div>
-          <div className={styles.Mobile}>
+            {/* Welcome */}
+          </motion.div>
+          <motion.div className={styles.Mobile} variants={pageVariants}>
             <span><p>Hi, I'm Scott</p></span>
-          </div>
+          </motion.div>
         </Container>
 
         <Container>
@@ -21,11 +34,15 @@ export default function Hero() {
         </Container>
 
         <Container>
-          <h5>Full Stack Developer and Sound Engineer based in Toronto, Canada.</h5>
+          <motion.div variants={pageVariants}>
+            <h5>Full Stack Developer and Sound Engineer based in Toronto, Canada.</h5>
+          </motion.div>
         </Container>
 
         <Container>
-          <Social />
+          <motion.div variants={pageVariants}>
+            <Social />
+          </motion.div>
         </Container>
       </div>
     </div>
