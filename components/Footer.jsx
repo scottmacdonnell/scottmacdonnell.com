9436ed34ebcee629eec71809a68708fe3b675e61
Scott MacDonnell <scottsmacdonnell@gmail.com>
2021-04-27 14:31:45 -0400

    Add Spotify NowPlaying component to Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 
 import Container from './Container'
+import NowPlaying from './NowPlaying'
 
 import styles from '../styles/components/Footer.module.scss'
 
@@ -8,13 +9,24 @@ export default function Footer() {
   return (
     <div className={styles.Footer}>
       <Container>
-        <span><p>Contact</p></span>
-        <h2>Have something in mind?</h2>
-        <h2>Let’s make it happen.</h2>
-        <br/>
-        <Link href="mailto:scott@scottmacdonnell.com">
-          <a><h5>scott@scottmacdonnell.com</h5></a>
-        </Link>
+        <div className={styles.FooterHeader}>
+          <div className={styles.FooterContact}>
+            <span><p>Contact</p></span>
+            <h2>Have something in mind?</h2>
+            <h2>Let’s make it happen.</h2>
+            <br/>
+            <Link href="mailto:scott@scottmacdonnell.com">
+              <a><h5>scott@scottmacdonnell.com</h5></a>
+            </Link>
+          </div>
+
+          <div className={styles.FooterSpotify}>
+            <div className={styles.FooterNowPlaying}>
+              <NowPlaying />
+            </div>
+            
+          </div>
+        </div>
       </Container>
       <Container>
         <div className={styles.Line} />
