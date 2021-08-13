7ef6ad3f4cf44c591c592809dbc2e6c679a74f0f
Scott MacDonnell <scottsmacdonnell@gmail.com>
2021-08-13 11:37:20 -0400

    Create Footer component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,6 @@
 import * as Page from '../components/Page'
+import * as Footer from '../components/Footer'
+import * as Social from '../components/Social'
 
 export default function Index() {
   return (
@@ -6,10 +8,40 @@ export default function Index() {
       title="Index"
     >
       <header></header>
+      
       <main>
         <h1>Scott MacDonnell</h1>
       </main>
-      <footer></footer>
+
+      <footer>
+        <Footer.Wrapper>
+          <Footer.Info>
+            <Footer.Tagline>
+              <Footer.Organization>
+                <Footer.OrganizationLogo>
+                  <Logo
+                    color="102, 102, 102"
+                    id="footer"
+                  />
+                </Footer.OrganizationLogo>
+
+                <Footer.OrganizationName>Scott MacDonnell</Footer.OrganizationName>
+              </Footer.Organization>
+
+              <Footer.Copyright>© 2021 Scott MacDonnell. All Rights Reserved.</Footer.Copyright>
+            </Footer.Tagline>
+
+            <Footer.SocialWrapper>
+              <Social.Wrapper>
+                <Social.Twitter username="macdonnellscott" />
+                <Social.GitHub username="scottmacdonnell" />
+                <Social.Instagram username="scott.macdonnell" />
+                <Social.LinkedIn username="scottsmacdonnell" />
+              </Social.Wrapper>
+            </Footer.SocialWrapper>
+          </Footer.Info>
+        </Footer.Wrapper>
+      </footer>
     </Page.Wrapper>
   )
 }
