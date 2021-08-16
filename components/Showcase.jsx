67e22252428342570a05bebc1fc42ebfe06eb478
Scott MacDonnell <scottsmacdonnell@gmail.com>
2021-08-16 15:01:09 -0400

    Add Showcase component

---
+++ components/Showcase.jsx
@@ -0,0 +1,82 @@
+import Image from 'next/image'
+import * as Text from './ui/Text'
+import styles from '../styles/components/Showcase.module.scss'
+
+export const Wrapper = (props) => {
+  return (
+    <div className={styles.Wrapper}>
+      <div className={styles.Showcase} {...props} />
+    </div>
+  )
+}
+
+export const FeaturedRight = (props) => {
+  return <div className={styles.FeaturedRight} {...props} />
+}
+
+export const FeaturedLeft = (props) => {
+  return <div className={styles.FeaturedLeft} {...props} />
+}
+
+export const SecondaryOnly = (props) => {
+  return <div className={styles.SecondaryOnly} {...props} />
+}
+
+export const FeaturedWrapper = (props) => {
+  return <div className={styles.FeaturedWrapper} {...props} />
+}
+
+export const SecondaryWrapper = (props) => {
+  return <div className={styles.SecondaryWrapper} {...props} />
+}
+
+export const Featured = (props) => {
+  return (
+    <Text.Anchor
+      href={props.href}
+      isExternal
+    >
+      <div className={styles.Featured}>
+        <div className={styles.Image}>
+          <Image
+            src={props.img}
+            alt={props.name}
+            layout="fill"
+            objectFit="cover"
+            objectPosition="center"
+          />
+        </div>
+        <div className={styles.Overlay}>
+          <Text.Heading6>{props.name}</Text.Heading6>
+          <Text.Paragraph style={{ color: '#666' }}>{props.href.replace(/(https:\/\/)/gm, '')}</Text.Paragraph>
+        </div>
+      </div>
+    </Text.Anchor>
+  )
+}
+
+export const Secondary = (props) => {
+  return (
+    <Text.Anchor
+      href={props.href}
+      isExternal
+    >
+      <div className={styles.Secondary}>
+        <div className={styles.Image}>
+          <Image
+            src={props.img}
+            alt={props.name}
+            width={960}
+            height={540}
+            objectFit="cover"
+            objectPosition="center"
+          />
+        </div>
+        <div className={styles.Overlay}>
+          <Text.Heading6>{props.name}</Text.Heading6>
+          <Text.Paragraph style={{ color: '#666' }}>{props.href.replace(/(https:\/\/)/gm, '')}</Text.Paragraph>
+        </div>
+      </div>
+    </Text.Anchor>
+  )
+}
